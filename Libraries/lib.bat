2451f19c31af9dab830ab252657935c1ca3d191c
DeepakYadav <deepakdv15@gmail.com>
2021-02-01 15:19:13 +0530

    added allure library

--- Libraries/lib.bat
+++ Libraries/lib.bat
@@ -1,6 +1,7 @@
-pip install robotframework==3.2.2
-pip install robotframework-pythonlibcore==2.2.1
-pip install robotframework-seleniumlibrary==4.5.0
-pip install selenium==3.141.0
-pip install urllib3==1.26.3
+python -m pip install robotframework==3.2.2
+python -m pip install robotframework-pythonlibcore==2.2.1
+python -m pip install robotframework-seleniumlibrary==4.5.0
+python -m pip install selenium==3.141.0
+python -m pip install urllib3==1.26.3
+python -m pip install allure_robotframework
 
